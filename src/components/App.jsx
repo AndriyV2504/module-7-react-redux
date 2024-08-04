576c2758da489a09603c4d63842f39a5589472be
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-04 11:39:10 +0300

    AppBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
+import { AppBar } from "./AppBar/AppBar";
+import { Layout } from "./Layout/Layout";
+
 const App = () => {
   return (
     <div>
       <h1>HELLO!!!</h1>
+      <Layout>
+        <AppBar />
+      </Layout>
     </div>
   );
 };
